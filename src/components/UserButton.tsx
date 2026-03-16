a1cb75485161f522c29aa7191398dc94647f1f6c
25LP0860 25LP0860 <25LP0860@G304-CC0222.local>
2026-03-16 09:55:38 +0800

    ss

--- src/components/UserButton.tsx
+++ src/components/UserButton.tsx
@@ -3,7 +3,8 @@ import { UserButton as ClerkUserButton } from "@clerk/nextjs";
 export function UserButton() {
   return (
     <div className="flex items-center gap-4">
-      <ClerkUserButton afterSignOutUrl="/" />
+
+      <ClerkUserButton />
     </div>
   );
 }
